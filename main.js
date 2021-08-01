022d64c2626968affe8543f2da2ec4106634bf6e
Joshua Wolf <jdwolf@student.fullsail.edu>
2021-08-01 13:12:45 -0400

    Removed console logs, added function for new email capture. Added dynamics by changing visiblity and display throughout.

--- main.js
+++ main.js
@@ -1,69 +1,72 @@
-//Let user know that we would like to reward them for learning about us
-alert("Thank you for reading about us! We would like to give you a discount");
-
 //declare arrays globally
 let discountArray = ["10%", "25%", "50%", "80%"];
 let expositionArray = ["1-5", "6-10","11-15", "16-20"]
+let message = document.querySelector("h4");
+let testCheckBox = document.getElementById("critCheck").checked = false;
+let checkBoxEvent = document.getElementById("critCheck").onclick = checkBox;
 
-console.log("Discounts are as follows:")
-for(let i =0; i < discountArray.length; i++)
-{
-    console.log(`For rolls ${expositionArray[i]}, you get a ${discountArray[i]} discount.`)
-}
+alert("Thanks for checking us out, see the bottom of the page for a reward!");
 
-console.log("If you roll a 20, you will get a second roll. If that second roll is also 20, then you get a free shirt of your choosing!");
-//confirm the user would like to participate
-if(confirm("Would you like to roll for a discount? You can view the page console for an explanation after your roll."))
-{
-    //run dice roll function
-    DiscountAlg();
-}
+//Roll Die On Button Press
+let buttonPress = document.getElementById('rollDie').onclick = DiscountAlg;
+
+//When email is submitted
+let submit = document.getElementById("submitEmail").onclick = EmailDiscount;
 
 //hold's array and discount criteria
 function DiscountAlg()
 {
-    //array for percentage discount won
-    discountArray = ["10%", "25%", "50%", "80%"];
-
+    if (message.style.color === "red")
+    {
+        message.style.color = "white";
+    }
     //variable runs function to return int for the roll
     let roll = RandomD20();
-    console.log(roll);
 
     //cases used for each tier of discount based on user roll
     //last case is for when the user rolls 20, it rolls to confirm. If 20 is rolled again, they get a free shirt
     let x = roll;
-    switch(true)
+    if (testCheckBox)
     {
-        case (x<=5):
-            DiscountMessage(discountArray[0], roll);
-            break;
-        case (x >=6 && roll <11):
-            DiscountMessage(discountArray[1], roll);
-            break;
-        case (x >=11 && roll <16):
-            DiscountMessage(discountArray[2], roll);
-            break;
-        case (x >=16 && roll <20):
-            DiscountMessage(discountArray[3], roll);
-            break;
-        case (x === 20):
-            let roll2 = RandomD20();
-            console.log(roll2);
-            if(roll2 === 20)
-            {
-                alert("Congratulations! You got a critical hit, you win a free shirt!");
-            }
-            else
-            {
+        message.style.color = "red";
+        message.innerHTML = "Congratulations! You got a critical hit, you win a free shirt!";
+    }else if(!testCheckBox){
+        switch(true)
+        {
+            case (x<=5):
+                DiscountMessage(discountArray[0], roll);
+
+                break;
+            case (x >=6 && roll <11):
+                DiscountMessage(discountArray[1], roll);
+
+                break;
+            case (x >=11 && roll <16):
+                DiscountMessage(discountArray[2], roll);
+
+                break;
+            case (x >=16 && roll <20):
                 DiscountMessage(discountArray[3], roll);
-            }
-            break;
+
+                break;
+            case (x === 20):
+
+                let roll2 = RandomD20();
+
+                if(roll2 === 20)
+                {
+                    message.style.color = "red";
+                    message.innerHTML = "Congratulations! You got a critical hit, you win a free shirt!";
+                }
+                else
+                {
+                    DiscountMessage(discountArray[3], roll);
+                }
+                break;
+        }
     }
 
-
 }
-
-alert("Discounts will automatically be applied in your cart! Thank you for your support!");
 
 //rolls a simulated 20 sided die and returns the number
 function RandomD20()
@@ -72,7 +75,34 @@ function RandomD20()
 }
 
 //outputs the winnings to the user
-function DiscountMessage(discount, roll)
+function DiscountMessage(d, r)
 {
-    alert(`Congratulations! You rolled a ${roll} for a ${discount} discount on a valid item!`);
+    message.innerHTML = `Congratulations! You rolled a ${r} for a ${d} discount on a valid item!`;
+    document.getElementById("winnings").style.visibility = 'visible';
+    document.getElementById("rollDie").style.visibility = 'hidden';
+    document.getElementById("claimDiscount").style.display = 'block';
+}
+
+function checkBox()
+{
+    if (testCheckBox === false)
+    {
+        testCheckBox = true;
+    }else
+    {
+        testCheckBox = false;
+    }
+}
+
+function EmailDiscount()
+{
+    let input = document.getElementById("userEmail").value;
+    if (input === "")
+    {
+        message.innerHTML = "Please enter your email!";
+    }else
+    {
+        document.getElementById("claimDiscount").style.display = 'none';
+        message.innerHTML = `Thank you! Your discount code has been sent to your provided email at ${input}.`;
+    }
 }
